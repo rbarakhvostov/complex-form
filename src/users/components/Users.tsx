577afbb6f2132aa2ffdb57f98206c00af7a2ac3a
rbarakhvostov <rbarakhvostov@gmail.com>
2024-09-17 15:31:50 +0300

    connect zod to RHF

--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -1,3 +1,31 @@
+import { useForm } from "react-hook-form";
+import { TextField, Stack } from "@mui/material";
+import { Schema, schema } from "../types/schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 export function Users() {
-  return <div>users</div>;
+  const {
+    register,
+    formState: { errors },
+  } = useForm<Schema>({
+    mode: "all",
+    resolver: zodResolver(schema),
+  });
+
+  return (
+    <Stack sx={{ gap: 2 }}>
+      <TextField
+        {...register("name")}
+        label="Name"
+        error={!!errors.name}
+        helperText={errors.name?.message}
+      />
+      <TextField
+        {...register("email")}
+        label="Email"
+        error={!!errors.email}
+        helperText={errors.email?.message}
+      />
+    </Stack>
+  );
 }
