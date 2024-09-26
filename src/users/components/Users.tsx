947d98bb70c6b9c46f495941661d6d7d4e03cb7d
rbarakhvostov <rbarakhvostov@gmail.com>
2024-09-26 13:30:04 +0300

    add the ability to clean up the form

--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -1,6 +1,6 @@
-import { Button, Stack } from "@mui/material";
+import { Button, Container, Stack } from "@mui/material";
 import { useFieldArray, useFormContext, useWatch } from "react-hook-form";
-import { Schema } from "../types/schema";
+import { defaultValues, Schema } from "../types/schema";
 import { RHFAutocomplete } from "../../components/RHFAutocomplete";
 import { Fragment, useEffect } from "react";
 import {
@@ -24,7 +24,7 @@ export function Users() {
   const gendersQuery = useGenders();
   const skillsQuery = useSkills();
 
-  const { watch, control, unregister } = useFormContext<Schema>();
+  const { watch, control, unregister, reset } = useFormContext<Schema>();
 
   useEffect(() => {
     const { unsubscribe } = watch((value) => console.log(value));
@@ -46,56 +46,63 @@ export function Users() {
     }
   }, [isTeacher, replace, unregister]);
 
+  const handleReset = () => reset(defaultValues);
+
   return (
-    <Stack sx={{ gap: 2 }}>
-      <RHFTextField<Schema> name="name" label="Name" />
-      <RHFTextField<Schema> name="email" label="Email" />
-      <RHFAutocomplete<Schema>
-        name="states"
-        label="States"
-        options={statesQuery.data}
-      />
-      <RHFToggleButtonGroup<Schema>
-        name="languages"
-        options={languagesQuery.data}
-      />
-      <RHFRadioGroup<Schema>
-        name="gender"
-        options={gendersQuery.data}
-        label="Gender"
-      />
-      <RHFCheckbox<Schema>
-        name="skills"
-        options={skillsQuery.data}
-        label="Skills"
-      />
-      <RHFDateTimePicker<Schema>
-        name="registrationDateTime"
-        label="Registration Date & Time"
-      />
-      <RHFDateRangePicker<Schema>
-        name="formerEmploymentPerion"
-        label="Former Employment Period:"
-      />
-      <RHFSlider<Schema> name="salaryRange" label="Salary Range" />
-      <RHFSwitch<Schema> name="isTeacher" label="Are you a teacher?" />
+    <Container maxWidth="sm" component="form">
+      <Stack sx={{ gap: 2 }}>
+        <RHFTextField<Schema> name="name" label="Name" />
+        <RHFTextField<Schema> name="email" label="Email" />
+        <RHFAutocomplete<Schema>
+          name="states"
+          label="States"
+          options={statesQuery.data}
+        />
+        <RHFToggleButtonGroup<Schema>
+          name="languages"
+          options={languagesQuery.data}
+        />
+        <RHFRadioGroup<Schema>
+          name="gender"
+          options={gendersQuery.data}
+          label="Gender"
+        />
+        <RHFCheckbox<Schema>
+          name="skills"
+          options={skillsQuery.data}
+          label="Skills"
+        />
+        <RHFDateTimePicker<Schema>
+          name="registrationDateTime"
+          label="Registration Date & Time"
+        />
+        <RHFDateRangePicker<Schema>
+          name="formerEmploymentPerion"
+          label="Former Employment Period:"
+        />
+        <RHFSlider<Schema> name="salaryRange" label="Salary Range" />
+        <RHFSwitch<Schema> name="isTeacher" label="Are you a teacher?" />
 
-      {isTeacher && (
-        <Button onClick={() => append({ name: "" })} type="button">
-          Add new student
-        </Button>
-      )}
+        {isTeacher && (
+          <Button onClick={() => append({ name: "" })} type="button">
+            Add new student
+          </Button>
+        )}
 
-      {fields.map((field, index) => (
-        <Fragment key={field.id}>
-          <RHFTextField name={`students[${index}].name`} label="Name" />
-          <Button
-            color="error"
-            onClick={() => remove(index)}
-            type="button"
-          ></Button>
-        </Fragment>
-      ))}
-    </Stack>
+        {fields.map((field, index) => (
+          <Fragment key={field.id}>
+            <RHFTextField name={`students[${index}].name`} label="Name" />
+            <Button color="error" onClick={() => remove(index)} type="button">
+              Remove
+            </Button>
+          </Fragment>
+        ))}
+
+        <Stack sx={{ flexDirection: "row", justifyContent: "space-between" }}>
+          <Button type="submit">New user</Button>
+          <Button onClick={handleReset}>Reset</Button>
+        </Stack>
+      </Stack>
+    </Container>
   );
 }
